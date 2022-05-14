b390f98596ef605a9cf83a5b9be8a13aa83f9003
shunsuke-tamura <wwwxx4869@gmail.com>
2022-05-14 14:04:44 +0900

    昨日のハンドラの作成

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include # include 追加
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 追加
-    path('', include('todo.urls')),
+    path('todo/', include('todo.urls')),
 ]
